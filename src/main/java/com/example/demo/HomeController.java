35a453744695d15c449754dd6be595d503462cef
wawdse <aiwdoao@gmail.com>
2026-01-29 16:50:44 +0900

    fix

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home page";
+    }
+}
