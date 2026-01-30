4116a0c88d5c69e171edbbf529ad2fea4618272d
wawdse <aiwdoao@gmail.com>
2026-01-30 10:16:34 +0900

    fix

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home page";
+        return "home page 2";
     }
 }
