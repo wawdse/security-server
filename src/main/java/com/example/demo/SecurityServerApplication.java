d3ed45d39db305342ea07423e7d91f84f031967d
wawdse <aiwdoao@gmail.com>
2026-01-30 11:17:07 +0900

    fix2

--- src/main/java/com/example/demo/SecurityServerApplication.java
+++ src/main/java/com/example/demo/SecurityServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServerApplication.class, args);
+    }
 
 }
