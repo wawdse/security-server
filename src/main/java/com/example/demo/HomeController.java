4aaa2c71d1eb203b21cdea5d47d145d3a0df7d66
wawdse <aiwdoao@gmail.com>
2026-01-30 16:45:04 +0900

    자동배포추가2

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home page 2";
+        return "home page2";
     }
 }
